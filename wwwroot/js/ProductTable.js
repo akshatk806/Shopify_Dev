4c318a675599e9599f9408b5fbeee962b6f76aeb
akshat.kumar <akshat.kumar@successive.tech>
2024-01-31 18:51:39 +0530

    Category wise filter using dropdown in table

--- wwwroot/js/ProductTable.js
+++ wwwroot/js/ProductTable.js
@@ -2,6 +2,24 @@ $(document).ready(function () {
     $('#myTable').DataTable({
         "order": [],
         "lengthMenu": [[100, 200, 300, -1], ["100", "200", "300", "All"]],
-        "pageLength": 100
+        "pageLength": 100,
+        "bFilter": true,
+    });
+
+    var oTable;
+    oTable = $('#myTable').dataTable();
+
+    $('#FilterByCategory').on('change', function () {
+        var selectedCategory = $(this).val();
+
+        // Loop through each row in the table body
+        $('#myTable tbody tr').each(function () {
+            var categoryInRow = $(this).find('td:eq(3)').text(); // Assuming "Category" is at index 3
+            if (selectedCategory === '' || categoryInRow === selectedCategory) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
     });
 }); 

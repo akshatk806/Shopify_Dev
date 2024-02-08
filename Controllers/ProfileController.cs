76f3da8fc26ed4391c1afd553dbe948b158b5844
akshat.kumar <akshat.kumar@successive.tech>
2024-02-08 23:07:36 +0530

    completed UI of user profile

---
+++ Controllers/ProfileController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Product_Management.Data;
+using Product_Management.Models.DomainModels;
+
+namespace Product_Management.Controllers
+{
+    [Route("Profile")]
+    public class ProfileController : Controller
+    {
+        private readonly AuthDbContext context;
+        public ProfileController(AuthDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Index([FromRoute] string Id)
+        {
+            var user = context.Users.FirstOrDefault(x => x.Id == Id);
+            if(user != null)
+            {
+                var profile = new UserModel()
+                {
+                    Name = user.Name,
+                    Email = user.Email,
+                    Phone = user.Phone,
+                    Address = user.Address,
+                    Password = user.Password,
+                    UserAddedAt = user.UserAddedAt
+                };
+                return View(profile);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
